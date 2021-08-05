7c7883b08a18cbdad8548dd1fc393fbe09fba179
Overlord <leon.pa.adolfo@gmail.com>
2021-08-05 10:06:00 -0500

    Ejercicios agregados

---
+++ src/SemanaCuatro/DooBee.java
@@ -0,0 +1,18 @@
+/*Given the output:
+        DooBeeDooBeeDo
+        Fill in the missing code:
+*/
+public class DooBee {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+
+        if (x == 3) {
+            System.out.print("Do");
+        }
+    }
+}
